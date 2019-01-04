341a812e24fb7536216840af39d78c5773b848ae
MATEUSZ KOCUR <93mateuszkocur@gmail.com>
2019-01-04 17:34:07 +0100

    Init

---
+++ TaskManager/TaskManager.Entities/Models/Enumerations/TaskStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Entities.Models.Enumerations
+{
+    public enum TaskStatus
+    {
+        ToDo,
+        InProgress,
+        Done,
+        OnHold,
+        Cancelled
+    }
+}
---
+++ TaskManager/TaskManager.Entities/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManager.Entities.Models.Enumerations;
+
+namespace TaskManager.Entities.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public TaskStatus TaskStatus { get; set; }
+    }
+}
